fe71ec523ce05ec15d89344f288f8c54231a1bb9
joshpaulie <joshpaullee@gmail.com>
2023-05-02 14:48:18 -0500

    ✨ App now actually serves data

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", current_time=dt.datetime.utcnow())
 
 
 if __name__ == "__main__":
